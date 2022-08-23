7d093e4dfbcb711cefc1f2614ec471e9001f5352
ibrahim-practice <100591745+ibrahim-practice@users.noreply.github.com>
2022-08-23 22:42:52 +0200

    Update secondeOne.js

--- secondeOne.js
+++ secondeOne.js
@@ -1,3 +1,20 @@
 // Write a JavaScript program to get the extension of a filename.
 
 const getfilename = (str) => str.slice(str.lastIndexOf(".")); 
+
+
+/*
+Write a javascript program to replace every character
+in a given string with the character following it in the 
+alphabet
+*/
+// string.fromCharCode
+// charCodeAt
+
+const moveCharsForward = (str) =>
+  str
+    .split("")
+    .map((char) => String.fromCharCode(char.charCodeAt(0) + 1))
+    .join("");
+
+console.log(moveCharsForward("abcd"));
