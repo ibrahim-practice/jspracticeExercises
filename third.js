83431a24112cad2a3cc4e9376f704599dd73b04b
ibrahim-practice <100591745+ibrahim-practice@users.noreply.github.com>
2022-08-24 11:14:20 +0200

    Update third.js

--- third.js
+++ third.js
@@ -1,1 +1,8 @@
+/*
+  write a javascript program to find the number of even
+  digits in an array of integers
+*/
+const evendigits = (str) => str.filter((chars) => chars % 2 === 0).length;
+
+console.log(evendigits([1, 3, 58, 12, 25, 38]));
 
