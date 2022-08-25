2848ef2583270eae568a43dcd0852e3c82f332f9
ibrahim-practice <100591745+ibrahim-practice@users.noreply.github.com>
2022-08-25 07:19:23 +0200

    Update fourth.js

--- fourth.js
+++ fourth.js
@@ -11,3 +11,19 @@ const objectsEqual = (a, b) => Object.keys(a).every((key) => b[key]);
 console.log(objectsEqual({ a: 1, b: 2, c: 4 }, { a: 1, b: 2, c: 4 }));
 console.log(objectsEqual({ a: 1, b: 2, c: 4 }, { a: 1, b: 2, d: 4 }));
 console.log(objectsEqual({ a: 1, b: 2, d: 4 }, { a: 1, b: 2, d: 4 }));
+
+
+/*
+    Write a javascript function that returns a passed
+    string with letters in alphabetical order.
+*/
+
+const alphabeticalOrder = (str) =>
+  str
+    .split("")
+    .sort((a, b) => (a > b ? 1 : -1))
+    .join("");
+
+console.log(alphabeticalOrder("gdfjhtr"));
+console.log(alphabeticalOrder("gdfhjhgrtdf"));
+console.log(alphabeticalOrder("qsgsgqsgg"));
